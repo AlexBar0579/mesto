5a15a8d5b24e3b31ba22f00ee37dc9af3a512327
Alex Bar <alexbar0579@yandex.ru>
2022-03-11 00:02:53 +0300

    ПР№4 итоговая версия для проверки №3

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,25 +3,27 @@ const openButtonEdit = document.querySelector('.profile__button-edit');
 const popupButtonClose = popup.querySelector('.popup__button-close');
 let profileName = document.querySelector('.profile__name');
 let profileProfession = document.querySelector('.profile__profession');
-let popupInputName = popup.querySelector('.popup__input_name');
-let popupInputProfession = popup.querySelector('.popup__input_profession');
-let popupSave = popup.querySelector('.popup__button-save');
+let popupInputName = popup.querySelector('.popup__input_type_name');
+let popupInputProfession = popup.querySelector('.popup__input_type_profession');
+let popupForm = document.querySelector('.popup__content');
 
-function togglePopup() {
-  popup.classList.toggle('popup_edit');
-};
-popup.addEventListener('click', function(event){
-  if (event.target === event.currentTarget) {
-    togglePopup();
-  }
-});
-function popupBottonSave(evt) {
+function openPopup() {
+  popup.classList.add('popup_edit');
+  popupInputName.value = profileName.textContent;
+  popupInputProfession.value = profileProfession.textContent;
+}
+
+function closePopup() {
+  popup.classList.remove('popup_edit');
+}
+
+function savePopupForm(evt) {
   evt.preventDefault();
   profileName.textContent = popupInputName.value;
   profileProfession.textContent = popupInputProfession.value;
-  togglePopup();
+  closePopup();
 };
-openButtonEdit.addEventListener('click', togglePopup);
-popupButtonClose.addEventListener('click', togglePopup);
-popupSave.addEventListener('click', popupBottonSave);
 
+openButtonEdit.addEventListener('click', openPopup);
+popupButtonClose.addEventListener('click', closePopup);
+popupForm.addEventListener('submit', savePopupForm);
